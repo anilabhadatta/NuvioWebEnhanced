0205fd2e017c18d67f744aaf5c53e443d77d87ab
Anilabha Datta <anilabhadatta@gmail.com>
2026-07-02 04:07:51 +0530

    added vercel analytics and speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import AppProviders from "@/components/AppProviders";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -26,6 +28,8 @@ export default function RootLayout({
         <AppProviders>
           {children}
         </AppProviders>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
